201ecb9e82a3242b7324880a76953e7bb44ff761
Tristan <deaugustinetristan@gmail.com>
2019-09-23 12:49:01 -0400

    fixed axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import MovieApiGrab from './components/Movie-db'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <MovieApiGrab />
   }
 }
 
